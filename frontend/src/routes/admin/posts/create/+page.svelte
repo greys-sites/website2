70fde40d566af5dac1bb9d8b3a04587bc38e5fe9
greysdawn <greysdawn@gmail.com>
2024-04-10 08:24:22 -0400

    fix: typo

--- frontend/src/routes/admin/posts/create/+page.svelte
+++ frontend/src/routes/admin/posts/create/+page.svelte
@@ -93,7 +93,7 @@
 			placeholder={!stags.length ? "Enter tags..." : ""}
 		/>
 	</div>
-	<label><input type="checkbox" name="pinned" />Pin post??</label>
+	<label><input type="checkbox" name="pinned" />Pin post?</label>
 	<input type="submit" value="Submit">
 </form>
 
