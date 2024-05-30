d08f0630f289c90ca7ce6849dad375a50e2ed534
greysdawn <greysdawn@gmail.com>
2024-05-30 17:50:05 -0400

    feat: blog post webhooks for discord

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,4 +1,5 @@
 import { Route } from './__models.js';
+import axios from 'axios';
 
 export default class PostRoutes extends Route {
 	constructor(app) {
@@ -54,6 +55,28 @@ export default class PostRoutes extends Route {
 			if(!data.hid) data.hid = data.title.replace(' ', '-').slice(0, 10).toLowerCase();
 
 			var post = await this.app.stores.posts.create(data);
+			if(process.env.POST_HOOK) {
+				await axios.post(process.env.POST_HOOK, {
+					embeds: [{
+						title: "New blog post!",
+						image: {
+							url: post.cover_url?.length ? post.cover_url : "https://cdn.greysdawn.com/img/81fa.png"
+						},
+						timestamp: (new Date).toISOString(),
+						color: 0x111111,
+						fields: [
+							{
+								name: post.title,
+								value: post.short
+							},
+							{
+								name: "Link",
+								value: `https://greysdawn.com/blog/${post.hid}`
+							}
+						]
+					}]
+				})
+			}
 			return res.status(200).send(post);
 		})
 
